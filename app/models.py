c3bbeced8a431f0ae87536bb4feed8936f36d815
TonyKioko <tonykioko384@gmail.com>
2018-10-04 17:05:24 +0300

    add methods to profile class

--- app/models.py
+++ app/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    profilePic = models.ImageField(upload_to='profpics/',default='NO IMAGE')
+    profilePhoto = models.ImageField(upload_to='profpics/',default='NO IMAGE')
     bio = models.CharField(max_length=60,blank=True)
     # user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
