8a3462cf405e2eeea96a22262bd9bbabd1951135
TonyKioko <tonykioko384@gmail.com>
2018-10-04 20:01:53 +0300

    image model added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
 
@@ -19,3 +21,28 @@ class Profile(models.Model):
     def get_profile(cls):
         profile = Profile.objects.all()
         return profile
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads/', default='NO IMAGE')
+    caption = models.CharField(max_length = 80)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # profile = models.ForeignKey(Profile,on_delete=models.CASCADE
+
+    def __str__(self):
+        return self.caption
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,caption):
+        updated_caption = Image.objects.filter(id=id).update(caption = caption)
+        return updated_caption
+
+    class Meta:
+        ordering = ['-timestamp']
