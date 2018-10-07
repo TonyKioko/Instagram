6a3a26655da03ebf1afe06600849461cf41e5d4c
TonyKioko <tonykioko384@gmail.com>
2018-10-07 14:27:21 +0300

    tests added

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,83 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+import datetime as dt
+from django.utils import timezone
+
+
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        # self.user = User(username='tony')
+        self.profile = Profile(profilePhoto='black and white',bio='test bio')
+        self.profile.save_profile()
+
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.image = Image(image ='image.url', caption='moringa',likes=1)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+    def test_delete_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
+
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.image=Image(caption="test iamge",likes=1)
+        self.image.save_image()
+
+        self.new_comment=Comment(comment="new comment",timestamp=timezone.now(),image=self.image)
+        self.new_comment.save_comment()
+
+    def tearDown(self):
+        Comment.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
+
+    def test_save_comment(self):
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments)>0)
+
+    def test_delete_comment(self):
+        self.new_comment.save_comment()
+        self.new_comment.delete_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments)==0)
