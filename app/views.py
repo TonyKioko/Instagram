41244f370cde839680781d665de8e4a94c8493a8
TonyKioko <tonykioko384@gmail.com>
2018-10-08 23:32:46 +0300

    sign up emailing

--- app/views.py
+++ app/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .emails import send_activation_email
 import smtplib
 
 
@@ -22,22 +23,25 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            current_user = form.save(commit=False)
-            current_user.is_active = False
-            current_user.save()
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
             current_site = get_current_site(request)
-            mail_subject = 'Activate your pixargram account.'
-            message = render_to_string('registration/acc_active_email.html', {
-                'user': current_user,
-                'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
-                'token':account_activation_token.make_token(current_user),
-            })
             to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                        mail_subject, message, to=[to_email]
-            )
-            email.send()
+            send_activation_email(user, current_site, to_email)
+
+            # mail_subject = 'Activate your pixargram account.'
+            # message = render_to_string('registration/acc_active_email.html', {
+            #     'user': current_user,
+            #     'domain': current_site.domain,
+            #     'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
+            #     'token':account_activation_token.make_token(current_user),
+            # })
+            # to_email = form.cleaned_data.get('email')
+            # email = EmailMessage(
+            #             mail_subject, message, to=[to_email]
+            # )
+            # email.send()
 
             return HttpResponse('Please confirm your email address to complete the registration')
     else:
@@ -47,13 +51,13 @@ def signup(request):
 def activate(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
-        current_user = User.objects.get(pk=uid)
+        user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        current_user = None
-    if current_user is not None and account_activation_token.check_token(current_user, token):
-        current_user.is_active = True
-        current_user.save()
-        login(request, current_user)
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
         # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. ''<a href="/accounts/login/"> Now you can login to your account </a>''')
     else:
